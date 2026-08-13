6cfce9e28c74f9db433ebbc6210837e021edc40f
Samruddhi-Nale <samruddhinale2007@gmail.com>
2026-08-13 12:46:14 +0000

    Temperature

---
+++ Temperature.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<array>
+using namespace std;
+int main(){
+    const int NUM_DAYS = 7;
+    array<double, NUM_DAYS> Ftrmp;
+    for(int i=0; i<NUM_DAYS; i++){
+        cout<<"Enter the temperature for day "<<i+1<<": ";
+        cin>>Ftrmp[i];
+    }
+    cout<<"temp in a week (in Celsius): "<<endl;
+    for(double temp : Ftrmp){
+        double tempC =(temp - 32)*5/9;
+        cout<<"F:"<<temp<<" C:"<<tempC<<endl;
+    }
+    return 0;
+}
