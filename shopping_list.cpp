ec01528cc9f01f1979c91085f374ad43443019eb
Samruddhi-Nale <samruddhinale2007@gmail.com>
2026-08-13 13:14:48 +0000

    shopping_list

--- shopping_list.cpp
+++ shopping_list.cpp
@@ -1,3 +1,6 @@
+// used vector of string to hold dynamic list of items
+//used sentinel controlled(user control to finsih loop) while loop
+//vectors are powerful tool to store data of unknown size
 #include<iostream>
 #include<vector>
 #include<string>
@@ -5,7 +8,15 @@ using namespace std;
 int main(){
     vector<string>ShoppingList;
     string item;
-    cout<<"Enter items for your shopping list (type 'done' to finish):"<<endl
-    while()
+    cout<<"Enter items for your shopping list (type 'done' to finish):"<<endl;
+    while(item != 'done'){
+        ShoppingList.push_back(item);
+        cout<<"Enter item(or 'done' to finish): "<<endl;
+        getline(cin, item);
+    }
+    cout<<"Your shopping list:"<<endl;
+    for(string item : ShoppingList){
+        cout<<item<<endl;
+    }
     return 0;
 }
